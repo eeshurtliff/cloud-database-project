d55f55c3c575ea0c324f51f2be6b46a88e306d5a
eeshurtliff <eeshurtliff@gmail.com>
2024-09-27 23:24:30 -0600

    started collecting links

---
+++ firebase_practice.py
@@ -0,0 +1,7 @@
+
+# https://firebase.google.com/docs/firestore/manage-data/add-data#python
+
+# https://firebase.google.com/docs/firestore/quickstart
+
+# https://youtu.be/p9pgI3Mg-So?si=NxX5QSJIOUKfy6Hy
+
