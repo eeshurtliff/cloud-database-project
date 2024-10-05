ec81e6b868716820f2d4b3954c507a15bbc96173
eeshurtliff <eeshurtliff@gmail.com>
2024-10-05 00:11:04 -0600

    got python code working!

--- clinics.py
+++ clinics.py
@@ -1,7 +1,71 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+def alter_document(db, collection_name, document_name, attributes_list, changed_attribute, value):
+    
+    results = db.collection(collection_name).document(document_name).get()
+    current_values = results.to_dict()
+    print(current_values)
+    if changed_attribute in attributes_list:
+        if type(value) == attributes_list[changed_attribute]:
+
+            print(current_values[changed_attribute])
+            current_values[changed_attribute] = value
+            print(current_values)
+            return current_values
+        else:
+            print('This is the wrong value type fr this attribute.')
+            return None
+    else:
+        print('This attribute does not exist in this document')
+        return None
+
+def create_student_database():
+    pass
+
+def main():
+
+
+    cred = credentials.Certificate("tennis_clinic_key.json")
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+
+    clinic_attributes = {'total students': int, 'student limit' : int, 'Day' : list}
+
+    new_values = alter_document(db, 'Clinics', 'Ages 11-13',clinic_attributes, 'Day', ['Monday', 'Wednesday', 'Friday'] )
+    db.collection("Clinics").document("Ages 11-13").set(new_values)
+
+
+
+    # result = db.collection("Clinics").get()
+    # total = 0
+    # for record in result:
+    #     data = record.to_dict()
+    #     print(record.id)
+
+    # choice = input("Select a clinic: ")
+    # result = db.collection("Clinics").document(choice).get()
+    # data = result.to_dict()
+    # if data is None:
+    #     print("Invalid class")
+    # else:
+    #     print(f" {data['Total Students']} / {data['Student Limit']}")
+
+
+    # choice = input("Select a clinic: ")
+    # new_limit = int(input("What is the new size limit for this class: "))
+    # db.collection("Clinics").document(choice).set({"Total Students" : 1, "Time" : "3:30pm", "Day" : "MWF", "Student Limit" : new_limit})
+
+
+    # db.collection("Clinics").document("Ages 11-13").set({"Total Students" : 3, "Time" : "3:30pm", "Day" : "TRS", "Student Limit" : 6})
+
+
+    # results = db.collection("Students").get()
+    # for result in results:
+    #     data = result.to_dict()
+    #     print(f"name = {data['First Name']}")
+
+if __name__ == "__main__":
+    main()
---
+++ research.md
@@ -0,0 +1,13 @@
+# Research Links
+
+- [Initialize Cloud Firestore: ](https://firebase.google.com/docs/firestore/manage-data/add-data#python) Gives code for how to connect your database to a code file and code examples of what can be done with that database
+
+- [Get started with Cloud Firestore: ](https://firebase.google.com/docs/firestore/quickstart#python) Shows how to set up your development and databse enviornments
+
+- [Introduction to Firebase Local Emulator Suite](https://firebase.google.com/docs/emulator-suite?hl=en&authuser=0)
+
+- [O'Reilly- Databse Options in the Cloud: ](https://www.oreilly.com/library/view/an-introduction-to/9781492044857/ch01.html)
+
+- [Firebase Realtime Database Creation](https://www.google.com/search?q=how+to+make+a+firebase+database&rlz=1C1CHBF_enUS989US989&oq=how+to+start+a+firebase+&gs_lcrp=EgZjaHJvbWUqCAgBEAAYFhgeMgYIABBFGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIKCAYQABgKGBYYHjIICAcQABgWGB4yCAgIEAAYFhgeMggICRAAGBYYHtIBCTEwMzUzajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:39388994,vid:qKxisFLQRpQ,st:0)
+
+- [Stack Abuse: ](https://stackabuse.com/python-check-if-variable-is-a-list/#checkifvariableisalistwithtype) Shows how to check to see if a variable is a list.
